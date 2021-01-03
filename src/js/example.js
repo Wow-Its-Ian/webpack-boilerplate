f6d7e2b57b26ebddbccfee597a51de7aeba6048a
Ian Walls <wallsianh@gmail.com>
2021-01-02 19:52:32 -0500

    fixed export syntax in example.js

--- src/js/example.js
+++ src/js/example.js
@@ -1,4 +1,4 @@
-export const example = () => {
+export default function example() {
   return (
     'Sensible webpack 5 boilerplate using Babel and PostCSS ' +
     'with a hot dev server and an optimized production build.'
